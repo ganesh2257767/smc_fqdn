8f7acee696b8c253ec0eeea05fb2defc7b041e98
Ganesh <ganeshkutty2257767@gmail.com>
2024-05-05 22:50:52 +0530

    Got the lookup to work, will add delete later

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ columns = [
     {'name': 'fqdn', 'label': 'FQDN', 'field': 'fqdn', 'required': True, 'align': 'center'},
     {'name': 'dn', 'label': 'DNs', 'field': 'dn', 'align': 'center'},
 ]
-rows = [
-    {'gw_name': '1003866', 'fqdn': 'v00152F2877BA.ny77.voip.eng.cv.net', 'dn': '5164923932, 5164923933', 'mac': '00152F2877BA'},
-]
+# rows = [
+#     {'gw_name': '1003866', 'fqdn': 'v00152F2877BA.ny77.voip.eng.cv.net', 'dn': '5164923932, 5164923933', 'mac': '00152F2877BA'},
+# ]
 
 
 @contextmanager
@@ -44,6 +44,7 @@ def show_hide_label(label: ui.label):
 
 async def login_smc(button: ui.button = None, spinner: ui.spinner = None, label: ui.label = None, stepper: ui.stepper = None):
     print('login_smc called')
+    global smc_obj
     data = []
     with requests.Session() as session:
         session.mount("https://", HTTPAdapter())
@@ -56,22 +57,40 @@ async def login_smc(button: ui.button = None, spinner: ui.spinner = None, label:
                         await run.io_bound(smc_obj.login, url_login, login_body)
                         label.text = 'Setting switch to DS5'
                         await run.io_bound(smc_obj.set_ds5_switch, url_set_ds5_switch, ds5_body)
-                        label.text ="Getting gateway names"
-                        await run.io_bound(smc_obj.get_gw_names_list, url_get_all_resi_gw)
-                        stepper.next()
+                        
                         # ui.notify('Logged in', position='center', type='positive')
         except requests.exceptions.ConnectionError as e:
             print("Exception occurred, quitting!", e)
-
+            ui.notify(f"Network exception occurred, please make sure VPN is connected and try again.", type='negative')
+            ui.notify(f"{type(e)} - {str(e)}", type='negative')
+        else:
+            stepper.next()
 
 async def get_all_gateways(button: ui.button = None, spinner: ui.spinner = None, label: ui.label = None, stepper: ui.stepper = None):
     with disable_enable_button(button):
         with start_loading_animation(spinner):
             with show_hide_label(label):
-                await run.io_bound(requests.get, 'https://httpbin.org/delay/1')
+                label.text = "Getting gateway names"
+                await run.io_bound(smc_obj.get_gw_names_list, url_get_all_resi_gw)
                 ui.notify('Done getting all Gateways', position='center', type='positive')
                 stepper.next()
 
+
+async def get_mac_fqdn(mac: str, button: ui.button = None, spinner: ui.spinner = None, label: ui.label = None, stepper: ui.stepper = None, table: ui.table = None):
+    with disable_enable_button(button):
+        with start_loading_animation(spinner):
+            with show_hide_label(label):
+                label.text = f"Getting FQDN details for mac: {mac}."
+                await run.io_bound(smc_obj.main, mac)
+                rows = []
+                if smc_obj.result:
+                    for result in smc_obj.result:
+                        print(result)
+                        rows.append({'gw_name': result[0], 'fqdn': result[1], 'dn': ', '.join(result[2]), 'mac': mac})
+                print(smc_obj.result)
+                print(rows)
+                table.rows = rows
+                stepper.next()
 
 def reset(stepper: ui.stepper):
     stepper.previous()
@@ -80,25 +99,29 @@ def reset(stepper: ui.stepper):
 
 with ui.stepper().props('vertical').classes('w-full') as stepper:
     with ui.step('Login to SMC'):
-        ui.button('Login', on_click=lambda e: login_smc(button=e.sender, spinner=spinner1, label=label1, stepper=stepper))
-        label1 = ui.label('')
-        spinner1 = ui.spinner('dots', size='lg', color='red')
-        label1.visible = False
-        spinner1.visible = False
-    with ui.step('Get all DNs'):
-        ui.button('Get', on_click=lambda e: get_all_gateways(e.sender, spinner2, label2, stepper))
-        label2 = ui.label('Please wait, getting all Gateways...')
-        spinner2 = ui.spinner('dots', size='lg', color='red')
-        label2.visible = False
-        spinner2.visible = False
+        ui.button('Login', on_click=lambda e: login_smc(button=e.sender, spinner=login_spinner, label=login_label, stepper=stepper))
+        login_label = ui.label('')
+        login_spinner = ui.spinner('dots', size='lg', color='red')
+        login_label.visible = False
+        login_spinner.visible = False
+    with ui.step('Get all Gateways'):
+        ui.button('Get', on_click=lambda e: get_all_gateways(e.sender, get_all_gw_spinner, get_all_gw_label, stepper))
+        get_all_gw_label = ui.label('Please wait, getting all Gateways...')
+        get_all_gw_spinner = ui.spinner('dots', size='lg', color='red')
+        get_all_gw_label.visible = False
+        get_all_gw_spinner.visible = False
     with ui.step('MAC'):
-        ui.input(label='', placeholder='Enter your MAC here',
+        mac_input = ui.input(label='', placeholder='Enter your MAC here',
          validation={'MAC should be 12 characters!': lambda value: len(value) == 12})
         with ui.stepper_navigation():
-            ui.button('Next', on_click=stepper.next)
-            ui.button('Back', on_click=stepper.previous, color='primary')
+            mac_check_button = ui.button('Check', on_click=lambda e: get_mac_fqdn(mac_input.value, mac_check_button, mac_spinner, mac_label, stepper, table = table))
+            mac_back_button = ui.button('Back', on_click=stepper.previous, color='primary')
+        mac_label = ui.label('')
+        mac_spinner = ui.spinner('dots', size='lg', color='red')
+        mac_label.visible = False
+        mac_spinner.visible = False
     with ui.step('Result'):
-        ui.table(columns=columns, rows=rows, row_key='name')
+        table = ui.table(columns=columns, rows=[{}], row_key='name')
         with ui.stepper_navigation():
             ui.button('Delete', color='red')
             ui.button('Back', on_click=stepper.previous, color='primary')
